ca00cedc147b4b893f6e026466043dc4ce93e2f5
akarimin <ali.nouri@paradigmquest.com>
2020-08-09 17:11:35 -0400

    sessions

---
+++ src/main/java/org/maktabkhooneh/core/introduction/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.maktabkhooneh.core.introduction;
+
+public class HelloWorld {
+    /**
+     * My First Java Program !
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
